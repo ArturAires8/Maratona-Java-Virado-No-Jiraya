8f9886da217706e24233561f8f27892c40d393b8
Artur Aires <arturaires8@gmail.com>
2025-12-03 12:08:32 -0300

    Aula 04 Operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -7,8 +7,25 @@ public class Aula04Operadores {
 
         int numero1 = 10;
         int numero2 = 20;
-        int resultado = numero1 / numero2;
+        double resultado = numero1 / (double) numero2;
         System.out.println(resultado);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " + isDezIgualVinte);
+        System.out.println("isDezIgualDez " + isDezIgualDez);
+        System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
 
     }
 }
