2ac9fa55e9b0d7f7d45c316ee1b3a5e455f9ea45
Artur Aires <arturaires8@gmail.com>
2025-12-03 11:44:40 -0300

    Aula03 Tipos Primitivos Exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+
+    public static void main(String[] args) {
+
+        String nome = "Artur";
+        String endereco = "Avenida Bela Vista, número 1560, condominio Gran Olyumps";
+        double salario = 5500.12;
+        String dataRecebimento = "03/12/2025";
+        String relatorio = "Eu " + nome + ", morando no endereço " + endereco +
+                ", confirmo que recebi o salário de R$ " + salario + ", na data " + dataRecebimento;
+        System.out.println(relatorio);
+
+    }
+
+}
