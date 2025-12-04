e343d9ca42e67705af8cde456d3eeeae0ca973fc
Artur Aires <arturaires8@gmail.com>
2025-12-04 14:34:51 -0300

    Aula 05 Estruturas Condicionais

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -5,7 +5,7 @@ public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
 
         double salario = 6000;
-        
+
         // (condicao) ? verdadeiro : falso
         String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" :"Ainda não tenho condições, mas vou ter!";
 
