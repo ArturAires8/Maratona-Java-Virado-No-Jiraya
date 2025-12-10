eacd92a260512e6bfd008c8752e577bdee1b4151
Artur Aires <arturaires8@gmail.com>
2025-12-10 11:36:55 -0300

    Orientação Objetos Referência de objetos

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -7,20 +7,28 @@ public class CarroTest01 {
     public static void main(String[] args) {
 
         Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
         carro1.nome = "Palio";
         carro1.modelo = "Fiat";
         carro1.ano = 2025;
 
-        System.out.println("Nome: " + carro1.nome + " Modelo: " + carro1.modelo + " Ano: " + carro1.ano);
-
-        System.out.println("-----------------------------------------------------------------------------------");
-
-        Carro carro2 = new Carro();
         carro2.nome = "Sandero";
         carro2.modelo = "Renault";
         carro2.ano = 2025;
 
-        System.out.println("Nome: " + carro2.nome + " Modelo: " + carro2.modelo + " Ano: " + carro2.ano);
+        carro1 = carro2;
+
+        System.out.println("Carro 1");
+        System.out.println(carro1.nome);
+        System.out.println(carro1.modelo);
+        System.out.println(carro1.ano);
+
+        System.out.println("\nCarro 2");
+        System.out.println(carro2.nome);
+        System.out.println(carro2.modelo);
+        System.out.println(carro2.ano);
+
 
     }
 
