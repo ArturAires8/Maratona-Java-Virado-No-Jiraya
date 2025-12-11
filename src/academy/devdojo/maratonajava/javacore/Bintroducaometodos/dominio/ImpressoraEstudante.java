dac54fa257d4ca60e8e299ae09a48e8a72ebf2c5
Artur Aires <arturaires8@gmail.com>
2025-12-11 15:47:05 -0300

    Orientação Objetos parte 08

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -5,7 +5,6 @@ public class ImpressoraEstudante {
     public void imprime(Estudante estudante) {
         System.out.println("-------------------------");
 
-
         System.out.println(estudante.nome);
         System.out.println(estudante.sexo);
         System.out.println(estudante.idade);
