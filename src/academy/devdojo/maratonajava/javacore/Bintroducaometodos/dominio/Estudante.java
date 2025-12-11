c133260858b92f8be734b15617701744869cd437
Artur Aires <arturaires8@gmail.com>
2025-12-11 15:25:26 -0300

    Orientação Objetos parte 06

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Estudante {
+
+    public String nome;
+    public int idade;
+    public char sexo;
+
+
+}
